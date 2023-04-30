bf19b1a09f7d0b6ba94f113ba4c98144260ebdb3
18608 <18608@LAPTOP-2H7AESPG>
2023-04-29 20:16:06 -0400

    Test

--- src/main/java/com/qa/utils/ExcelLib.java
+++ src/main/java/com/qa/utils/ExcelLib.java
@@ -1,5 +1,55 @@
 package com.qa.utils;
 
-public class ExcelLib {
+import java.io.File;
+import java.io.FileInputStream;
 
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelLib 
+{
+	XSSFWorkbook test1;
+	//constructor
+	public ExcelLib(String ExcelPath)
+	{
+	try 
+	{
+		File source = new File(ExcelPath);
+		FileInputStream test = new FileInputStream(source);
+		test1 = new XSSFWorkbook(test);
+	}
+	catch(Exception e)
+	
+	{
+		System.out.println("Unable to load the excel file"+e.getMessage());
+	}
+	
 }
+	//to count the row numbers of a sheet using excel sheet number
+	public int getRowCount(int sheetnum)
+	{
+		int count = test1.getSheetAt(sheetnum).getLastRowNum()+1;
+		return count;
+	}
+	//to count the row numbers of a sheet using excel sheet name
+	public int getRowCount(String sheetName)
+	{
+		int count = test1.getSheet(sheetName).getLastRowNum()+1;
+		return count;
+	}
+	
+	//to get data of a cell using sheet number
+	public String getCellData(int sheetnum, int rowNum, int colNum)
+	{
+		String data = test1.getSheetAt(sheetnum).getRow(rowNum).getCell(colNum).toString();
+		return data;
+	}
+	//to get data of a cell using sheet name
+	public String getCellData(String sheetname, int rowNum, int colNum)
+	{
+		String data = test1.getSheet(sheetname).getRow(rowNum).getCell(colNum).toString();
+		return data;
+	}
+	
+}
+
--- src/test/java/com/qa/testcases/BaseTest.java
+++ src/test/java/com/qa/testcases/BaseTest.java
@@ -2,10 +2,12 @@ package com.qa.testcases;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
 import com.qa.pages.EnterPage;
 import com.qa.pages.LoginPage;
 import com.qa.rsi.DriverScript;
+import com.qa.utils.ExcelLib;
 
 public class BaseTest extends DriverScript{
 
@@ -24,5 +26,22 @@ public class BaseTest extends DriverScript{
 	public void tearDown()
 	{
 		quitDriver();
+	}
+	@DataProvider(name="actilogin")
+	public Object[][] testdata()
+	{
+		ExcelLib excel = new ExcelLib("./atTestData/actiData.xlsx");
+		int count = excel.getRowCount(0);
+		
+		
+		Object[][] data = new Object[count][2];
+		for(int i=0;i<count;i++)
+		{
+		data[i][0]=excel.getCellData(0, i, 0);
+		data[i][1]=excel.getCellData(0, i, 1);
+		}
+		
+		
+	return data;	
 	}
 }
